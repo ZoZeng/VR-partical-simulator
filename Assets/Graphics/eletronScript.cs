660d51daf9d4f28a9368b5f8b51a02bb5e147633
ZoZeng <by.zer02nd@gmail.com>
2017-05-04 21:54:21 -0400

    VR

---
+++ Assets/Graphics/eletronScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class eletronScript : MonoBehaviour {
+	public static List<GameObject> list = new List<GameObject>();
+    
+    public Vector3 acc=new Vector3();
+
+	// Use this for initialization
+	void Start () {
+		list.Add(gameObject);
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate (){
+		foreach(GameObject g in list){
+			var distance= g.transform.position-gameObject.transform.position;
+			if(distance.magnitude>0.50){
+				acc += (distance.normalized*10f*g.GetComponent<Rigidbody>().mass)/(distance.magnitude*distance.magnitude);
+			}
+
+
+		}
+		foreach(GameObject g in ProjectileScript.list){
+			var distance= g.transform.position-gameObject.transform.position;
+			if(distance.magnitude>0.50){
+				acc += (distance.normalized*10f*g.GetComponent<Rigidbody>().mass)/(distance.magnitude*distance.magnitude);
+			}
+
+		}
+		foreach(GameObject g in protonSc.list){
+			var distance= g.transform.position-gameObject.transform.position;
+			if(distance.magnitude>0.50){
+				acc += (distance.normalized*10f*g.GetComponent<Rigidbody>().mass)/(distance.magnitude*distance.magnitude);
+			}
+
+		}
+
+		gameObject.GetComponent<Rigidbody> ().velocity += acc*Time.deltaTime;
+
+	}
+}
---
+++ Assets/Scripts/ShootScript.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using System.Linq;
+
+public class ShootScript : MonoBehaviour {
+	public GameObject eletronPrefab;
+    public GameObject projectilePrefab;
+	public GameObject protonPrefab;
+    public float speed;
+    public float startTime;
+    public float deltaTime;
+    private bool bottonWasDown;
+    private float bottonDownTime;
+    private int i=1;
+	// Use this for initialization
+	void Start () {
+		bottonWasDown=false;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+		var joystickNames = Input.GetJoystickNames ();
+
+		if (Input.GetButton ("Fire") || Input.GetButton ("Jump")) {
+			if(!bottonWasDown){
+				bottonDownTime = Time.time;
+			}
+			bottonWasDown = true;
+				
+			// Shoot stuff
+			//var projectile = Instantiate (projectilePrefab, transform.position, Quaternion.identity) as GameObject;
+			//var projBody = projectile.GetComponent<Rigidbody> ();
+			//projBody.velocity = transform.forward * speed;
+			//speed++;
+		} else if(bottonWasDown) {
+			bottonWasDown = false;
+			float dt = Time.time - bottonDownTime;
+			if(i%2==0){
+				var projectile = Instantiate (projectilePrefab, transform.position, Quaternion.identity) as GameObject;
+				var projBody = projectile.GetComponent<Rigidbody> ();
+				projBody.velocity = transform.forward * speed*dt;
+			}else if(i%3==0){
+				var proton = Instantiate (protonPrefab, transform.position, Quaternion.identity) as GameObject;
+				var protonBody = proton.GetComponent<Rigidbody> ();
+				protonBody.velocity = transform.forward * speed*dt;
+			}
+			else{
+				var eletron = Instantiate (eletronPrefab, transform.position, Quaternion.identity) as GameObject;
+				var eleBody = eletron.GetComponent<Rigidbody> ();
+				eleBody.velocity = transform.forward * speed*dt;
+			}
+			i++;
+
+		}
+
+        }
+	}
